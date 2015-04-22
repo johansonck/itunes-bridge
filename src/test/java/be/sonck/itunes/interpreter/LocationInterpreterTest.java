7a1306c70e6181f356ee2c92108da80a571050a9
Johan Sonck <johan.sonck@gmail.com>
2015-04-22 22:04:14 +0200

    falende test hersteld

--- src/test/java/be/sonck/itunes/interpreter/LocationInterpreterTest.java
+++ src/test/java/be/sonck/itunes/interpreter/LocationInterpreterTest.java
@@ -9,7 +9,7 @@ public class LocationInterpreterTest {
 
 	@Test
 	public void test() {
-		String location = "Macintosh HD 2:iTunes:iTunes Music:Music:Yo-Yo Ma:Cello Suites Nos. 1, 5 & 6:18 No. 6 - VI. Gigue.mp3";
+		String location = "Macintosh HD 2:iTunes:iTunes Music:Music:Yo-Yo Ma:Bach_ Cello Suites Nos. 1, 5 & 6:18 No. 6 - VI. Gigue.mp3";
 		File file = new LocationInterpreter().interpret(location);
 		Assert.assertNotNull(file);
 		Assert.assertTrue(file.exists());
