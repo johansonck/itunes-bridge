54269f4c2ae8ce4703d941876e599a698a0344de
Johan Sonck <johan.sonck@gmail.com>
2016-02-13 01:55:30 +0100

    simplified factory

--- src/main/java/be/sonck/itunes/api/service/ITunesBridgeFactory.java
+++ src/main/java/be/sonck/itunes/api/service/ITunesBridgeFactory.java
@@ -4,21 +4,7 @@ import be.sonck.itunes.impl.service.DefaultITunesBridge;
 
 public final class ITunesBridgeFactory {
 	
-	private static Class<? extends ITunesBridge> implementationClass = DefaultITunesBridge.class;
-	
-	
-	public static void setImplementationClass(Class<? extends ITunesBridge> implementationClass) {
-		ITunesBridgeFactory.implementationClass = implementationClass;
-	}
-
 	public static ITunesBridge create() {
-		try {
-			return implementationClass.newInstance();
-			
-		} catch (InstantiationException e) {
-			throw new RuntimeException(e);
-		} catch (IllegalAccessException e) {
-			throw new RuntimeException(e);
-		}
+		return new DefaultITunesBridge();
 	}
 }
